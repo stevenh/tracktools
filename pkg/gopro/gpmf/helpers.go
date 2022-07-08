1d2df100112f5bd19b8fb678b9028106157ff96e
Steven Hartland <steven.hartland@multiplay.co.uk>
2022-07-08 20:15:46 +0100

    feat(gpmf): add hero 10 face support
    Add support for Hero 10 face detection.
    
    This renames W -> Width and H -> Height as well a refactoring to have
    separate types for the different versions Hero face detection.

--- pkg/gopro/gpmf/helpers.go
+++ pkg/gopro/gpmf/helpers.go
@@ -5,30 +5,31 @@ import (
 	"strings"
 )
 
-// validateTypeDef validates that e has a type definition which matches size and typeDef.
-func validateTypeDef(e *Element, typeDefs map[string]byte) error {
+// validateTypeDef validates that e has a type definition contained in typeDefs and
+// returns the one it matches.
+func validateTypeDef(e *Element, typeDefs map[string]byte) (string, error) {
 	f := e.friendlyName()
 
 	td, ok := e.Metadata[friendlyName(KeyTypeDef)]
 	if !ok {
-		return fmt.Errorf("%s: missing type def", f)
+		return "", fmt.Errorf("%s: missing type def", f)
 	}
 
 	t, ok := td.(string)
 	if !ok {
-		return fmt.Errorf("%s: unexpected type def type %T (expected string)", f, td)
+		return "", fmt.Errorf("%s: unexpected type def type %T (expected string)", f, td)
 	}
 
 	types := make([]string, 0, len(typeDefs))
 	for k, v := range typeDefs {
 		if t == k {
 			if e.Header.Size != v {
-				return fmt.Errorf("%s: unexpected data size %d (expected %d)", f, e.Header.Size, v)
+				return "", fmt.Errorf("%s: unexpected data size %d (expected %d)", f, e.Header.Size, v)
 			}
-			return nil
+			return t, nil
 		}
 		types = append(types, k)
 	}
 
-	return fmt.Errorf("%s: unexpected type def type %q (expected one of %s)", f, t, strings.Join(types, ","))
+	return "", fmt.Errorf("%s: unexpected type def type %q (expected one of %s)", f, t, strings.Join(types, ","))
 }
